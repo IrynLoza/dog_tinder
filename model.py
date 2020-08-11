190ecae1ffdb70685e724913e8c0fc7ca7fbc0d8
Iryna Loza <irynloza@gmail.com>
2020-08-11 14:39:34 -0700

    Create data models

--- model.py
+++ model.py
@@ -12,9 +12,86 @@ def connect_to_db(flask_app, db_uri='postgresql:///ratings', echo=True):
 
     print('Connected to the db!')
 
+
+class User(db.Model):
+    """A user."""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)  
+    user_name = db.Column(db.String, unique=True, nullable=False) 
+    password = db.Column(db.String, nullable=False)                               
+    email = db.Column(db.String, unique=True, nullable=False)
+    breed = db.Column(db.String, nullable=True)
+    location = db.Column(db.String, nullable=True)
+    gender = db.Column(db.String, nullable=True)
+    summary = db.Column(db.Text, nullable=True)
+    preferences = db.Column(db.Text, nullable=True)
+
+
+    def __repr__(self):
+        return f'<User user_id={self.user_id} user_name={self.user_name} email={self.email}>' 
+       
+
+class Image(db.Model):
+    """A user images."""
+
+    __tablename__ = "images"
+
+    image_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)  
+    image_url = db.Column(db.String, nullable=False) 
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+
+    user = db.relationship('User', backref='images')
     
 
+    def __repr__(self):
+        return f'<User image_id={self.image_id} image_url={self.image_url}>' 
+       
 
+class Like(db.Model):
+    """A user likes."""
+
+    __tablename__ = "likes"
+
+    like_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)  
+    
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    user_id_of_liked_user = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+
+    current_user = db.relationship('User', backref='likes')
+    liked_user = db.relationship('User', backref='likes')
+
+
+    def __repr__(self):
+        return f'<User like_id={self.like_id} user_id={self.user_id} liked_user={self.user_id_of_liked_user}>' 
+
+class Dislike(db.Model):
+    """A user dislikes."""
+
+    __tablename__ = "dislikes"
+
+    dislike_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)  
+    
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    user_id_of_disliked_user = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+
+    cur_user = db.relationship('User', backref='dislikes')
+    disliked_user = db.relationship('User', backref='dislikes')    
+    
+
+    def __repr__(self):
+        return f'<User dislike_id={self.dislike_id} user_id={self.user_id} disliked_user={self.user_id_of_disliked_user}>' 
+ 
 
 
 if __name__ == '__main__':
