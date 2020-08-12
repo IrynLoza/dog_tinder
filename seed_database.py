7c1dbc11f18015876564b74a30ff9829a0eabd01
Iryna Loza <irynloza@gmail.com>
2020-08-12 13:57:57 -0700

    Seed users database

--- seed_database.py
+++ seed_database.py
@@ -17,12 +17,45 @@ model.db.create_all()
 #Generate fake data
 fake = Faker()
 
-for i in range(10):
+# generate locations:
+# fake.address()
+# fake.zipcode_in_state('CA')
+
+# generate dog name
+# fake.first_name()
+
+breed_list = [
+            'Mix', 'Welsh Corgi Pembroke', 'German Shepherd', 'Husky', 'Bulldog', 
+            'St. Bernard', 'Chow Chow', 'Labradoodle', 'Beagle', 'Labrador Retriever',
+            'Poodle', 'Pug', 'Border Colliie', 'Dachshund', 'Pomeranian', 'Doberman',
+            'Shiba Inu', 'Rottweiler', 'Chihuahuas', 'Yorkshire Terriers'
+            ]
+
+gender_list = ['male', 'femaale']
+summary_list = ['Friendly, fluffy friend', 'Adorable buddy for walk', 'Can be your best friend',
+            'Couch potato', 'New dog friends lover', 'Smart cookie', 'Love treats and dogs',
+            'Make your life better', 'Bring a lot of fun to your life', 'Show you the best dog park'
+            ]
+preferences_list = ['Looking for new friends', 'Obsessed with husky', 'Wanna play together',
+                'Looking for corgi for seriouse relations', 'Join my amazing company',
+                'How about shedding together?', 'Looking for you, yes, you', 
+                'Let me treats and I will love you', 'Show you my favorite spot', 
+                'Looking for seriouse partnership', 'Love pugs', 'Be my friend',
+                'Ready for everyone' 
+                ]        
+
+for i in range(100):
     user_name = fake.user_name()
     password = fake.password() 
     email = fake.email()
-
-    user = crud.create_user(user_name, password, email) 
+    location = fake.zipcode_in_state('CA') 
+    breed = choice(breed_list)
+    gender = choice(gender_list)
+    summary = choice(summary_list)
+    preferences = choice(preferences_list)
+    
+    user = crud.create_user(user_name, password, email, breed, location, gender, summary, 
+                            preferences) 
      
 
 # fake = Faker(['en_US'])
