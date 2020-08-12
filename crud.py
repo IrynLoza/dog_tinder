5476cc54d7a4c82181c87bc3d6cd0d0561680aea
Iryna Loza <irynloza@gmail.com>
2020-08-12 14:01:44 -0700

    Create get_users function

--- crud.py
+++ crud.py
@@ -1,14 +1,23 @@
 from model import db, User, Image, Like, Dislike, connect_to_db
 
-def create_user(user_name, password, email):
+def create_user(user_name, password, email, breed, location, gender,
+                summary, preferences):
     """Create and return a new user."""
 
-    user = User(user_name=user_name, password=password, email=email)
+    user = User(user_name=user_name, password=password, email=email, breed=breed, 
+                location=location, gender=gender, summary=summary,
+                preferences=preferences)
 
     db.session.add(user)
     db.session.commit()
 
     return user
+
+def get_users():
+    """Get all users objects"""
+
+    return User.query.all()
+        
 
 
 if __name__ == '__main__':
