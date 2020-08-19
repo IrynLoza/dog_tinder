da948d13a6ad35cc393fda87cc5ebb2063984db8
Iryna Loza <irynloza@gmail.com>
2020-08-18 20:04:17 -0700

    Created endpoints to get random user, likes, mathes

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, flash, session, redirect, jsonify
-from flask_jwt_extended import JWTManager, create_access_token
+from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity, jwt_required
 
 from model import connect_to_db
 from random import choice, randint
@@ -16,7 +16,6 @@ app.config['JWT_SECRET_KEY'] = os.environ['SECRET_KEY']
 app.jinja_env.undefined = StrictUndefined
 
 jwt = JWTManager(app)
-
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
@@ -52,45 +51,106 @@ def login():
 
 
 @app.route("/api/random-user")
+@jwt_required
 def get_random_user():
     """Get random user from database"""
 
-    user_id = randint(1,102)
+    current_id = get_jwt_identity()['user_id']
+    ignore_list = [current_id]
+
+    # TODO 
+    # 1 create the query with join and combine two functions
+    # 2 get count all records from users and use in randint
+    likes = crud.get_likes_by_user(current_id)
+    for like in likes:
+        ignore_list.append(like.target_user_id)
+
+    dislikes = crud.get_dislikes_by_user(current_id)
+    for dislike in dislikes:
+        ignore_list.append(dislike.target_user_id) 
+
+    while True:
+        user_id = randint(1,101)   
+        if user_id not in ignore_list:
+            break 
+
     user = crud.get_user_by_id(user_id)
     user_img = crud.get_user_img_by_id(user_id)
     response = user.serialize()
     response['user_img'] = user_img.serialize()
-    # result = []
-    # for user in users:
-    #     result.append(user.serialize())
+ 
 
     return jsonify(response)
 
 
-@app.route("/api/like", methods=['POST']) 
+@app.route("/api/like", methods=['POST'])
+@jwt_required
 def get_likes():
-    """Get likes from current user"""  
+    """Get likes from current user""" 
     data = request.get_json()
-
+    
     target_id = data['target_id']
-    current_user_id = session['user']['user_id']
+    current_user_id = get_jwt_identity()['user_id']
 
-    like = crud.create_like(current_user_id, target_id)
+    crud.create_like(current_user_id, target_id)
+
+    #check if target_id have ever liked current_id
+    #if yes - store to match
+    is_like = crud.get_likes_by_target_id(target_id, current_user_id)
+    
+    if is_like:
+        #place where match create
+        crud.create_match(current_user_id, target_id)
   
     return jsonify({'status': 'ok'})
 
 @app.route("/api/dislike", methods=['POST']) 
+@jwt_required
 def get_dislikes():
     """Get dislikes from current user"""  
     data = request.get_json()
 
     target_id = data['target_id']
-    current_user_id = session['user']['user_id']
+    current_user_id = get_jwt_identity()['user_id']
 
-    dislike = crud.create_dislike(current_user_id, target_id)
+    crud.create_dislike(current_user_id, target_id)
    
     return jsonify({'status': 'ok'})
 
+
+@app.route("/api/matches")
+@jwt_required
+def get_match():
+    """"""
+    
+    current_id = get_jwt_identity()['user_id']
+
+    #get all matches for current user
+    matches_list = crud.get_matches(current_id)
+    
+    #create unique list with current user matches
+    #if current user = match.user_id = add data to target_user_id column
+    #else opposite 
+    user_id_list = []
+    for match in matches_list:
+        if match.user_id == current_id:
+            user_id_list.append(match.target_user_id)
+        else:
+            user_id_list.append(match.user_id)
+
+    #remove all duplicates id's
+    user_id_set = set(user_id_list)
+
+    matches = crud.get_user_by_ids(list(user_id_set))
+    result = []
+    for match in matches:
+        user_img = crud.get_user_img_by_id(match.user_id)
+        user = match.serialize()
+        user['user_img'] = user_img.serialize()
+        result.append(user)
+
+
+    return jsonify(result) 
 
 
 if __name__ == '__main__':
