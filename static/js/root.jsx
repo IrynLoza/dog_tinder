7f6cb93f33696c077c7a3fda86377573d801102e
Iryna Loza <irynloza@gmail.com>
2020-09-15 21:16:26 -0700

    Update socket

--- static/js/root.jsx
+++ static/js/root.jsx
@@ -539,7 +539,7 @@ function Chat(props) {
 //*******ROUTES**********/
 //Create utility component to handle all routes except login page in client side
 function PrivateRoute() {
-    const socket = io()
+    const socket = io({path: "/mysocket"})
     const sessionKey = localStorage.getItem('session-key');
     const history = useHistory();
 
